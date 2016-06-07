cd1bf1e4c2dcd41553d7c8c5a408d432c8a74bc4
Michael Thessel <michael.thessel@gmx.de>
2016-06-06 21:11:17 -1000

    Added watchdog timer

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/wdt.h>
 #include <util/delay.h>
 #include "leds.h"
 #include "ir.h"
 
 int main(void)
 {
+    wdt_enable(WDTO_1S);
     leds_blink();
 
     ir_setup();
 
     while(1) {
-        ;
+        wdt_reset();
+
         //leds_run(10, 10);
 
         IR_TX_ON();
         _delay_us(10);
         IR_TX_OFF();
-        _delay_ms(1000);
+        _delay_ms(500);
     }
 
     return 0;
@@ -27,7 +30,7 @@ ISR(PCINT2_vect)
 {
     cli();
 
-    leds_blink();
+    //leds_blink();
 
     sei();
 }
