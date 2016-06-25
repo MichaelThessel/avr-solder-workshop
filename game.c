a44ab44e55b42675696cb54b609ca298068a68b1
Michael Thessel <michael.thessel@gmx.de>
2016-06-24 18:07:13 -1000

    Added basic blink sequence

--- game.c
+++ game.c
@@ -1,30 +1,64 @@
 #include <util/delay.h>
 #include "leds.h"
-#include "ir.h"
+
+uint8_t game_debounce_active = 0;
 
 void game_setup()
 {
-    // Set up IR
-    ir_setup();
+    DDRC &= ~(1 << DDC0); // Configure PC0 as input
+    DDRC &= ~(1 << DDC1); // Configure PC1 as input
+
+    PORTC |= (1 << PORTC0) | (1 << PORTC1); // Pull PC0 & PC1 high
+
+    PCICR |= (1 << PCIE1); // Enable pin change interrupts for [14:8]
+    PCMSK1 |= (1 << PCINT8) | (1 << PCINT9); // Enable pin change interrupt for PCINT8 & 9
+    EICRA |= (1 << ISC11); // Trigger pin change interrupt on falling edge
+
+    sei(); // Global interrupt enable
 }
 
 void game_loop()
 {
-    while (ir_is_active()) {
-        _delay_us(1);
-    }
-
-    uint8_t data[4] = {1, 1, 1, 0};
-    ir_tx_send(data);
-    _delay_ms(10);
+    leds_run(10, 10);
 }
 
-void ir_rx_success(uint8_t data[4])
+void game_button1_press()
 {
-    if (data[0] == 1 && data[1] == 1 && data[2] == 1 && data[3] == 0) {
-        struct cRGB color = {20, 0, 0}; // Green
-        leds_set(1, color);
-        _delay_ms(300);
-        leds_off();
+    struct cRGB color = {0, 20, 0};
+    leds_blink(color);
+}
+
+void game_button2_press()
+{
+    struct cRGB color = {20, 0, 0};
+    leds_blink(color);
+}
+
+// Enable debounce
+void game_debounce()
+{
+    game_debounce_active = 1;
+    TCCR0B |= (1 << CS00) | (1 << CS01); // prescaler 1024
+    TIMSK0 |= (1 << OCIE0A); // Enable timer compare match interrupt
+}
+
+// Handle button pushes
+ISR(PCINT1_vect)
+{
+    if (game_debounce_active) return;
+
+    if (PINC & (1 << PINC0)) {
+        game_button1_press();
+    } else if (PINC & (1 << PINC1)) {
+        game_button2_press();
     }
+
+    game_debounce();
+}
+
+// Disable debounce
+ISR(TIMER0_COMPA_vect)
+{
+    game_debounce_active = 0;
+    TIMSK0 &= ~(1 << OCIE0A);
 }
--- game.h
+++ game.h
@@ -7,3 +7,18 @@ void game_setup();
  * Game loop
  */
 void game_loop();
+
+/*
+ * Handle button1 press
+ */
+void game_button1_press();
+
+/*
+ * Handle button2 press
+ */
+void game_button2_press();
+
+/*
+ * Debounce button presses
+ */
+void game_debounce();
